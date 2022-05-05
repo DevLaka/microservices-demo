4d3b2d190e8126838cebb614868488cfb79fafab
devlaka <lakshanwarunab@gmail.com>
2022-05-05 17:21:53 +0530

    handle incoming events

--- query-service/index.js
+++ query-service/index.js
@@ -5,10 +5,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/posts", (req, res) => {});
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
 app.post("/events", (req, res) => {
-  console.log("Event Received: ", req.body.type);
+  const { type, data } = req.body;
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  res.send({ status: "OK" });
 });
 
 app.listen(5002, () => {
