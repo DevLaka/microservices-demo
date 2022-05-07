1f1704a26c3f311b864213dd9ee94dc4fc83598b
devlaka <lakshanwarunab@gmail.com>
2022-05-07 21:54:20 +0530

    filter comments

--- micro-demo-client/src/CommentList.js
+++ micro-demo-client/src/CommentList.js
@@ -2,7 +2,17 @@ import React from "react";
 
 export const CommentList = ({ comments }) => {
   const renderComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "Awaiting comment moderation";
+    }
+    if (comment.status === "rejected") {
+      content = "Comment has been rejected";
+    }
+    return <li key={comment.id}>{content}</li>;
   });
   return (
     <div className="d-flex flex-row flex-wrap justify-content-between">
