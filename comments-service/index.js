011d6f052013cde6f00a0b0a103a617de26a8e51
devlaka <lakshanwarunab@gmail.com>
2022-05-07 21:15:39 +0530

    add update comment status logic

--- comments-service/index.js
+++ comments-service/index.js
@@ -35,8 +35,26 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  console.log("Event Received: ", req.body.type);
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => comment.id === id);
+    comment.status = status;
+    await axios
+      .post("http://localhost:5010/events", {
+        type: "CommentUpdated",
+        data: {
+          id,
+          content,
+          postId,
+          status,
+        },
+      })
+      .catch((e) => console.log(e.message));
+    res.send("OK");
+  }
 });
 
 app.listen(5001, () => {
