4972dbd2a34878c1f3d8cb123fe6f0a31a4e5d15
devlaka <lakshanwarunab@gmail.com>
2022-05-08 07:31:49 +0530

    fix query service bug

--- query-service/index.js
+++ query-service/index.js
@@ -24,7 +24,7 @@ app.post("/events", (req, res) => {
   }
   if (type === "CommentUpdated") {
     const { id, content, postId, status } = data;
-    const post = post[postId];
+    const post = posts[postId];
     const comment = post.comments.find((comment) => comment.id === id);
     comment.status = status;
     comment.content = content;
