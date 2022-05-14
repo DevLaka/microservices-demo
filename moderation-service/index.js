66401653e4580ec21fbac1a4c1bd1d9acd6ca0ad
devlaka <lakshanwarunab@gmail.com>
2022-05-14 09:04:04 +0530

    dockerize services

--- moderation-service/index.js
+++ moderation-service/index.js
@@ -4,8 +4,6 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-const posts = {};
-
 app.post("/events", async (req, res) => {
   const { type, data } = req.body;
   if (type === "CommentCreated") {
